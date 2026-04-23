be1a6355bb6602c00ae1908b9f04d5670856e7c5
Stephan Kern <stephan@swmmrman.com>
2026-04-23 03:00:09 -0600

    Fun testing playing with LEDs, and budget PWM

--- fan_controller/src/main.rs
+++ fan_controller/src/main.rs
@@ -1,13 +1,15 @@
 #![no_std]
 #![no_main]
 
-use embassy_rp as hal;
 use embassy_executor::Spawner;
+use embassy_rp as hal;
 use embassy_rp::block::ImageDef;
+use embassy_rp::gpio;
 use embassy_time::Timer;
+use gpio::{Level, Output};
 
 //Panic Handler
-use {panic_probe as _};
+use panic_probe as _;
 // Defmt Logging
 use defmt_rtt as _;
 
@@ -19,10 +21,25 @@ pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
+    let mut leds: [Output; 8] = [
+        Output::new(p.PIN_6, Level::Low),
+        Output::new(p.PIN_7, Level::Low),
+        Output::new(p.PIN_9, Level::Low),
+        Output::new(p.PIN_8, Level::Low),
+        Output::new(p.PIN_10, Level::Low),
+        Output::new(p.PIN_11, Level::Low),
+        Output::new(p.PIN_12, Level::Low),
+        Output::new(p.PIN_13, Level::Low),
+    ];
+    loop {
+        for led in &mut leds {
+            led.set_high();
+            Timer::after_micros(2).await;
 
-    loop{
-        Timer::after_millis(100).await;
-    }   
+            led.set_low();
+            Timer::after_micros(200).await;
+        }
+    }
 }
 
 // Program metadata for `picotool info`.
@@ -31,9 +48,7 @@ async fn main(_spawner: Spawner) {
 #[used]
 pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
     embassy_rp::binary_info::rp_program_name!(c"fan_controller"),
-    embassy_rp::binary_info::rp_program_description!(
-        c"your program description"
-    ),
+    embassy_rp::binary_info::rp_program_description!(c"your program description"),
     embassy_rp::binary_info::rp_cargo_version!(),
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
